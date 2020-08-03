ff3e4790496698da34c7d9f27c04d67b85b78408
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-03 17:41:58 +0900

    deploy

--- shmovie/src/App.js
+++ shmovie/src/App.js
@@ -1,20 +1,5 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 
-function Food(props) {
-  console.log(props.name);
-  return <h1>I like me {props.name}</h1>;
-}
-
-class App extends Component {
-  render() {
-    return (
-      <>
-        <h1>hello</h1>
-        <Food name="kk" />
-      </>
-    );
-  }
-}
+function App() {}
 
 export default App;
