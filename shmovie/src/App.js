0540cb20ce9c7bd9802feff9ebcf733bb41448db
HanSangHyeok <tkdgur8377@gmail.com>
2020-08-05 18:17:43 +0900

    route추가

--- shmovie/src/App.js
+++ shmovie/src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
-
-function App() {}
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+function App() {
+  return (
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
+}
 
 export default App;
